13be89d81449f0ece9c34ac768fc0327c1c6e403
sobhanzeinali <sobhan.zeinali@gmail.com>
2020-03-23 20:59:59 +0430

    After DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,11 +2,24 @@
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private VidlyContext _context;
+
+        public CustomersController()
+        {
+            _context = new VidlyContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public Customer Customer { get; set; }
 
         // GET: ICustomers
@@ -19,17 +32,18 @@ namespace Vidly.Controllers
             //     new Customer() {Name = "JE", Id = 2}
             // };
             // var movieViewModel = new RandomMovieViewModel() {Customers = ICustomers, Movie = Movie};
-            var customer = GetCustomers();
+            var customer = _context.Customers.Include(c => c.MemberShipType).ToList();
             return View(customer);
         }
 
         public ActionResult Details(int id)
         {
-            Customer = GetCustomers().SingleOrDefault(i => i.Id == id);
-            if (Customer==null)
+            Customer = _context.Customers.SingleOrDefault(i => i.Id == id);
+            if (Customer == null)
             {
                 return HttpNotFound();
             }
+
             return View(Customer);
         }
 
